22cd50e518ce3b01ef4638bd8b8637410cd3ea68
RyotaOku <ryota1991o@gmail.com>
2023-10-24 12:23:54 +0900

    main

--- src/components/createStatus/result/status.tsx
+++ src/components/createStatus/result/status.tsx
@@ -3,7 +3,7 @@ import style from '@/styles/components/result/status.module.css'
 export default function Status() {
     return (
         <div>
-            <h3>ちんちん</h3>
+            <h3>64%独立性</h3>
         </div>
     )
 }
