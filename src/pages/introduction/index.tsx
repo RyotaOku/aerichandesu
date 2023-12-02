71599a9c9aeffd0400b41b6ba10d3dd41a476883
HondaYt <honda.yuto.04@gmail.com>
2023-12-02 12:58:24 +0900

    利用者の声開始

--- src/pages/introduction/index.tsx
+++ src/pages/introduction/index.tsx
@@ -1,4 +1,5 @@
 import style from '@/styles/introduction/index.module.css'
+import { type } from 'os';
 import { Link as Scroll } from 'react-scroll'
 
 
@@ -6,21 +7,21 @@ function getImagePath(filename: string) {
     return `/images/introduction/${filename}`;
 }
 
-type MainConceptProps = {
+type introFeatureProps = {
     children?: React.ReactNode,
     imgAlt: string,
     imgSrc: string,
 }
 
-export function MainConcept(props: MainConceptProps) {
+export function IntroFeature(props: introFeatureProps) {
 
     return (
-        <div className={style.mainConcept}>
-            <picture className={style.mainConceptImg}>
+        <div className={style.introFeatureChild}>
+            <picture className={style.introFeatureImg}>
                 <img src={getImagePath(props.imgSrc)}
                     alt={props.imgAlt} />
             </picture>
-            {/* <p className={style.mainConceptText}>{props.text}</p> */}
+            {/* <p className={style.introFeatureText}>{props.text}</p> */}
             {props.children}
         </div>
     )
@@ -43,7 +44,7 @@ export function IntroDetail(props: IntroDetailProps) {
     )
 }
 
-type FeatureProps = {
+type DetailedFunctionProps = {
     className?: string,
     title: string,
     text: string,
@@ -51,16 +52,39 @@ type FeatureProps = {
     imgSrc: string,
 }
 
-export function Feature(props: FeatureProps) {
+export function DetailedFunction(props: DetailedFunctionProps) {
     return (
-        <div className={style.feature}>
-            <picture className={style.featureImg}>
+        <div className={style.detailedFunction}>
+            <picture className={style.detailedFunctionImg}>
                 <img src={getImagePath(props.imgSrc)}
                     alt={props.imgAlt} />
             </picture>
-            <div className={style.featureText}>
+            <div className={style.detailedFunctionText}>
                 <h3>{props.title}</h3>
                 <p >{props.text}</p>
+            </div>
+        </div>
+    )
+}
+
+type ServiceDetailProps = {
+    children?: React.ReactNode,
+    title: string;
+    text: string;
+    imgAlt: string,
+    imgSrc: string,
+}
+
+export function ServiceDetail(props: ServiceDetailProps) {
+    return (
+        <div className={style.serviceDetail}>
+            <picture className={style.serviceDetailImg}>
+                <img src={getImagePath(props.imgSrc)} alt={props.imgAlt} />
+            </picture>
+            <div className={style.serviceDetailText}>
+                <h3>{props.title}</h3>
+                <p>{props.text}</p>
+                {props.children}
             </div>
         </div>
     )
@@ -69,73 +93,112 @@ export function Feature(props: FeatureProps) {
 export default function Introduction() {
 
     return (
-        <div className={style.wrap}>
+        <>
             <div className={style.mainVisual}>
-                <div className={style.firstView}>
-                    <nav className={style.navigation}>
-                        <h1>Service Name</h1>
-                        <ul>
-                            <li>
-                                <Scroll to='test' smooth>特徴</Scroll>
-                            </li>
-                            <li>利点</li>
-                            <li>利用者の声</li>
-                        </ul>
-                    </nav>
+                <nav className={style.navigation}>
+                    <h1>Service Name</h1>
+                    <ul>
+                        <li>
+                            <Scroll to='test' smooth>特徴</Scroll>
+                        </li>
+                        <li>利点</li>
+                        <li>利用者の声</li>
+                    </ul>
+                </nav>
 
 
+                <div>
                     <h2>あなたの魅力を最大限引き出す、
                         キャリアプラットフォーム。</h2>
                     <h3>自分を知り、未来を創る。</h3>
                     <p>これは単なるポートフォリオサポートツールではありません。あなたの未来、キャリア、夢を形にするためのマジックボックスです。</p>
                     <a href="#">今すぐ始めよう</a>
                 </div>
+                <picture><img src={getImagePath("mainVisual.svg")} alt="" /></picture>
             </div>
 
-            <div className={style.decorationWrap} >
-                <div className={style.decoration1}></div>
+            <div className={style.introFeaturesSection}>
+                <div className={style.introFeaturesWrap}>
+                    <IntroFeature imgAlt='' imgSrc='introFeature1.svg'>
+                        <p>ありのままに答えることで真のあなたの想いを引き出すことができます。</p>
+                    </IntroFeature>
+                    <IntroFeature imgAlt='' imgSrc='introFeature2.svg'>
+                        <p>道に迷ったなら何度でもここへ
+                            帰ってきてください。何度でも
+                            お手伝いします。</p>
+                    </IntroFeature>
+                    <IntroFeature imgAlt='' imgSrc='introFeature3.svg'>
+                        <p>自分のキャリアマップがわからないなら、自分を知ることから始めましょう。</p>
+                    </IntroFeature>
+                </div>
+                <h2>美しい言葉、魅力的なフレーズ、<br />あなたの真の価値を引き出すアドバイス。<br />すべてがここに。</h2>
             </div>
+            {/* <introFeature text={'道に迷ったなら何度でもここへ帰ってきてください。何度でもお手伝いします。'} imgAlt='' imgSrc='introFeature2.svg' /> */}
+            {/* <introFeature text={'自分のキャリアマップがわからないなら、自分を知ることから始めましょう。'} imgAlt='' imgSrc='introFeature3.svg' /> */}
 
-            <div className={style.mainConceptWrap}>
-                <MainConcept imgAlt='' imgSrc='mainConcept1.svg'>
-                    <p>ありのままに答えることで真のあなたの想いを引き出すことができます。</p>
-                </MainConcept>
-                <MainConcept imgAlt='' imgSrc='mainConcept2.svg'>
-                    <p>道に迷ったなら何度でもここへ
-                        帰ってきてください。何度でも
-                        お手伝いします。</p>
-                </MainConcept>
-                <MainConcept imgAlt='' imgSrc='mainConcept3.svg'>
-                    <p>自分のキャリアマップがわからないなら、自分を知ることから始めましょう。</p>
-                </MainConcept>
-                {/* <MainConcept text={'道に迷ったなら何度でもここへ帰ってきてください。何度でもお手伝いします。'} imgAlt='' imgSrc='mainConcept2.svg' /> */}
-                {/* <MainConcept text={'自分のキャリアマップがわからないなら、自分を知ることから始めましょう。'} imgAlt='' imgSrc='mainConcept3.svg' /> */}
+            <div className={style.serviceDetailsSection}>
+                <div className={style.serviceDetailsWrap}>
+                    <ServiceDetail
+                        imgSrc='sS1.png'
+                        imgAlt=''
+                        title="必要なのは、ほんの数ステップです。"
+                        text="いくつかの質問に答えるだけで、あなたのための、クオリティーの高いサポートを提供します。"
+                    />
+                    <ServiceDetail
+                        imgSrc='sS2.png'
+                        imgAlt=''
+                        title="あなたのことを教えてください。"
+                        text="GPT-4搭載の優れたAIがあなたの強みやキャリアビジョンを言語化。"
+                    ><div><p>produced by</p>
+                            <picture><img src={getImagePath("OpenAiLogo.svg")} alt="" /></picture>
+                        </div></ServiceDetail>
+                </div>
             </div>
-            <div>
-                <h2>美しい言葉、魅力的なフレーズ、あなたの真の価値を引き出すアドバイス。すべてがここに。</h2>
-                <div className={style.conceptWrap}>
-                    <picture className={style.conceptImg}>
-                        <img src={getImagePath("sS1.png")} alt="" />
-                    </picture>
-                    <div className={style.conceptText}>
-                        <h3>必要なのは、ほんの数ステップです。</h3>
-                        <p>いくつかの質問に答えるだけで、あなたのための、クオリティーの高いサポートを提供します。</p>
+            <div className={style.detailedFunctionsSection}>
+                <h2>これらの素晴らしい機能が、ひとつに。</h2>
+                <div className={style.detailedFunctionsWrap}>
+                    <div className={style.detailedFunctionsWrap}>
+                        <DetailedFunction title='優れたアシスタント' text='GPT-4を搭載した、優れたAIによって、あなたにあったビジョンを提案することができます。' imgAlt='' imgSrc='function1.svg' />
+                        <DetailedFunction title='美しいフレーズ' text='わたしたちで紡ぐ、美しい言葉があなたのポートフォリオを彩ります。' imgAlt='' imgSrc='function2.svg' />
+                    </div>
+                    <div className={style.detailedFunctionsWrap}>
+                        <DetailedFunction title='グラフで可視化' text='あなたの傾向を予測し、グラフで数値的に可視化。客観的な視点から、自分を見つめられるでしょう。' imgAlt='' imgSrc='function3.svg' />
+                        <DetailedFunction title='最適なアドバイス' text='さらなる高みを目指すあなたへ向けた、次のステップへ進むための道しるべ。' imgAlt='' imgSrc='function4.svg' />
                     </div>
                 </div>
-
-            </div>
-            <h2>これらの素晴らしい機能が、ひとつに。</h2>
-            <div className={style.featuresWrap}>
-                <div className={style.featuresWrap}>
-                    <Feature title='優れたアシスタント' text='GPT-4を搭載した、優れたAIによって、あなたにあったビジョンを提案することができます。' imgAlt='' imgSrc='feature1.svg' />
-                    <Feature title='美しいフレーズ' text='わたしたちで紡ぐ、美しい言葉があなたのポートフォリオを彩ります。' imgAlt='' imgSrc='feature2.svg' />
-                </div>
-                <div className={style.featuresWrap}>
-                    <Feature title='グラフで可視化' text='あなたの傾向を予測し、グラフで数値的に可視化。客観的な視点から、自分を見つめられるでしょう。' imgAlt='' imgSrc='feature3.svg' />
-                    <Feature title='最適なアドバイス' text='さらなる高みを目指すあなたへ向けた、次のステップへ進むための道しるべ。' imgAlt='' imgSrc='feature4.svg' />
-                </div>
             </div>
 
-        </div>
+            <div className={style.userTestimonialSection}>
+                <h2>利用者の声</h2>
+                <div>
+                    <div>
+                    </div>
+                    <div>
+                        <button></button>
+                        <button></button>
+                        <button></button>
+                        <button></button>
+                        <button></button>
+                        <button></button>
+                    </div>
+                </div>
+            </div>
+
+            <h2>さあ、準備は整いましたか？
+                私たちと一緒に、あなたのキャリアデザインを
+                素敵に彩りましょう！</h2>
+            <a href="#">今すぐ始めよう</a>
+
+            <footer className={style.footer}>
+                <ul>
+                    <li><a href='#'>プライバシー&利用規約</a></li>
+                    <li><a href='#'>お問い合わせ</a></li>
+                    <li><a href='#'>私たちについて</a></li>
+                </ul>
+
+                <p><small>Service Name.inc &copy; 2023</small></p>
+            </footer>
+
+        </>
     )
 }
