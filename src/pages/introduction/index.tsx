213b4719c4e04ad24e42a6c5971e83f2e3b88b5d
HondaYt <honda.yuto.04@gmail.com>
2023-12-04 02:37:11 +0900

    スタイル以外完了

--- src/pages/introduction/index.tsx
+++ src/pages/introduction/index.tsx
@@ -1,6 +1,8 @@
+import { useState, useEffect, useReducer } from 'react'
 import style from '@/styles/introduction/index.module.css'
 import { type } from 'os';
 import { Link as Scroll } from 'react-scroll'
+import { log } from 'console';
 
 
 function getImagePath(filename: string) {
@@ -69,8 +71,8 @@ export function DetailedFunction(props: DetailedFunctionProps) {
 
 type ServiceDetailProps = {
     children?: React.ReactNode,
-    title: string;
-    text: string;
+    title: string,
+    text: string,
     imgAlt: string,
     imgSrc: string,
 }
@@ -90,6 +92,133 @@ export function ServiceDetail(props: ServiceDetailProps) {
     )
 }
 
+// type userProps = {
+//     imgAlt: string,
+//     imgSrc: string,
+// }
+
+// export function UserButton(props: userProps) {
+//     return (
+//         <button className={style.userButton}>
+//             <picture className={style.userButtonImg}><img src={getImagePath(props.imgSrc)} alt={props.imgAlt} /></picture>
+//         </button>
+//     )
+// }
+
+
+
+
+type userProps = {
+    imgAlt: string,
+    imgSrc: string,
+    index: number,
+    onClick: (index: number) => void,
+    isActive: boolean,
+}
+
+const users = [
+    {
+        imgSrc: 'user1.svg',
+        imgAlt: 'user1',
+        name: 'Webエンジニア',
+        age: '20代',
+        gender: '女性',
+        comment: 'このサイトを使って診断し、Webエンジニアの道へ進むことを決断しました。それからは悩みに直面する度、ここで自分を見つめ直し、今では大手企業に就職することができました。このサービスにはとても感謝しています。',
+    },
+    {
+        imgSrc: 'user2.svg',
+        imgAlt: 'user2',
+        name: 'Webデザイナー',
+        age: '30代',
+        gender: '男性',
+        comment: 'このサイトの診断結果が私のキャリアチェンジのきっかけとなりました。Webデザイナーという選択肢を提示してくれ、それが私の新たな道となりました。今では自分のデザインが多くの人々に使われていることに大いなる喜びを感じています。',
+    },
+    {
+        imgSrc: 'user3.svg',
+        imgAlt: 'user3',
+        name: 'バックエンドエンジニア',
+        age: '40代',
+        gender: '女性',
+        comment: 'このサイトを通じて、自分がバックエンドエンジニアになりたいという想いを強くしました。診断結果が私の可能性を示してくれ、それが私のキャリアのステップアップにつながりました。',
+    },
+    {
+        imgSrc: 'user4.svg',
+        imgAlt: 'user4',
+        name: 'Webディレクター',
+        age: '30代',
+        gender: '女性',
+        comment: 'このサイトの診断を受けて、自分がWebディレクターになりたいという目標を見つけました。それからは、自分のリーダーシップと創造性を追求することができ、今では自分の指導が多くのプロジェクトを成功に導いていることに誇りを感じています。',
+    },
+    {
+        imgSrc: 'user5.svg',
+        imgAlt: 'user5',
+        name: 'フルスタックエンジニア',
+        age: '30代',
+        gender: '男性',
+        comment: 'このサイトの診断を受けて、自分がフルスタックエンジニアになりたいという目標を見つけました。それからは、自分の技術と情熱を追求することができ、今では自分のコードが多くのプロジェクトを成功に導いていることに誇りを感じています。',
+    },
+    {
+        imgSrc: 'user6.svg',
+        imgAlt: 'user6',
+        name: 'データサイエンティスト',
+        age: '20代',
+        gender: '女性',
+        comment: 'このサイトの診断結果が私のキャリアの方向性を見つけるのに大いに役立ちました。データサイエンティストという職業を選んだことで、自分のスキルを活かし、価値ある洞察を提供することができています。',
+    },
+];
+
+export function UserButton(props: userProps) {
+    return (
+        <button className={`${style.userButton} ${props.isActive ? style.active : ''}`} onClick={() => props.onClick(props.index)}>
+            <picture className={style.userButtonImg}><img src={getImagePath(props.imgSrc)} alt={props.imgAlt} /></picture>
+        </button>
+    )
+}
+
+export function UserTestimonial() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrevClick = () => {
+        setCurrentIndex((currentIndex - 1 + users.length) % users.length);
+    };
+
+    const handleNextClick = () => {
+        setCurrentIndex((currentIndex + 1) % users.length);
+    };
+
+    const handleUserButtonClick = (index: number) => {
+        setCurrentIndex(index);
+    };
+
+    const currentUser = users[currentIndex];
+
+    return (
+        <div className={style.userTestimonialSection}>
+            <h2>利用者の声</h2>
+            <div className={style.userTestimonial}>
+                <div className={style.userDetailWrap}>
+                    <button className={`${style.userDetailPrev} ${style.userDetailNavigation}`} onClick={handlePrevClick} />
+                    <div className={style.userDetailContent}>
+                        <picture className={style.userDetailImg}><img src={getImagePath(currentUser.imgSrc)} alt="" /></picture>
+                        <div className={style.userDetailText}>
+                            <h3>{currentUser.name}<span>{currentUser.age} {currentUser.gender}</span></h3>
+                            <p>{currentUser.comment}</p>
+                        </div>
+                    </div>
+                    <button className={`${style.userDetailNext} ${style.userDetailNavigation}`} onClick={handleNextClick} />
+                </div>
+                <div className={style.userButtonSection}>
+                    {users.map((user, index) => (
+                        <UserButton key={index} imgSrc={user.imgSrc} imgAlt={user.imgAlt} index={index} onClick={handleUserButtonClick} isActive={index === currentIndex} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
 export default function Introduction() {
 
     return (
@@ -107,14 +236,16 @@ export default function Introduction() {
                 </nav>
 
 
-                <div>
-                    <h2>あなたの魅力を最大限引き出す、
-                        キャリアプラットフォーム。</h2>
-                    <h3>自分を知り、未来を創る。</h3>
-                    <p>これは単なるポートフォリオサポートツールではありません。あなたの未来、キャリア、夢を形にするためのマジックボックスです。</p>
-                    <a href="#">今すぐ始めよう</a>
+                <div className={style.topView}>
+                    <div className={style.topViewText}>
+                        <h2>あなたの魅力を最大限引き出す、
+                            キャリアプラットフォーム。</h2>
+                        <h3>自分を知り、未来を創る。</h3>
+                        <p>これは単なるポートフォリオサポートツールではありません。あなたの未来、キャリア、夢を形にするためのマジックボックスです。</p>
+                        <a href="#">今すぐ始めよう</a>
+                    </div>
+                    <picture className={style.topViewImg}><img src={getImagePath("mainVisual.svg")} alt="" /></picture>
                 </div>
-                <picture><img src={getImagePath("mainVisual.svg")} alt="" /></picture>
             </div>
 
             <div className={style.introFeaturesSection}>
@@ -133,8 +264,6 @@ export default function Introduction() {
                 </div>
                 <h2>美しい言葉、魅力的なフレーズ、<br />あなたの真の価値を引き出すアドバイス。<br />すべてがここに。</h2>
             </div>
-            {/* <introFeature text={'道に迷ったなら何度でもここへ帰ってきてください。何度でもお手伝いします。'} imgAlt='' imgSrc='introFeature2.svg' /> */}
-            {/* <introFeature text={'自分のキャリアマップがわからないなら、自分を知ることから始めましょう。'} imgAlt='' imgSrc='introFeature3.svg' /> */}
 
             <div className={style.serviceDetailsSection}>
                 <div className={style.serviceDetailsWrap}>
@@ -168,22 +297,7 @@ export default function Introduction() {
                 </div>
             </div>
 
-            <div className={style.userTestimonialSection}>
-                <h2>利用者の声</h2>
-                <div>
-                    <div>
-                    </div>
-                    <div>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                    </div>
-                </div>
-            </div>
-
+            <UserTestimonial />
             <h2>さあ、準備は整いましたか？
                 私たちと一緒に、あなたのキャリアデザインを
                 素敵に彩りましょう！</h2>
