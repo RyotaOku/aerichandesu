fdfad558539037a24a1548705adc870582a8db6f
RyotaOku <ryota1991o@gmail.com>
2024-02-01 21:58:33 +0900

    main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 export default function Main() {
     return (
-        <p>開発中</p>
+        <>
+            <p>開発中</p>
+            <button onClick={() => {
+                window.open('https://localhost:50443/introduction', '_self');
+            }}></button>
+        </>
     )
 }
